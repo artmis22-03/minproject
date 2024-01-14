0cfaff834c24065245c0293dff21745b4c7e8322
artmis22-03 <sricham2203@gmail.com>
2024-01-14 16:27:53 +0530

    hi

--- website/auth.py
+++ website/auth.py
@@ -1,12 +1,19 @@
 from flask import Blueprint, render_template,request
+import mysql.connector
 
 auth=Blueprint('auth',__name__)
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
-    data=request.form
-    print(data)
+    
     return render_template("Login.html")
 
+
+@auth.route('/authenticate',methods=['GET','POST'])
+def authent():
+    
+    cur=mysql.connector.cursor()
+    # cur.execute("select * from users where password=%s",(password))
+    
 
 
