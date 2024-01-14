76a8306c0bd452a9c3fca25e7b143630412be603
artmis22-03 <sricham2203@gmail.com>
2024-01-14 11:37:45 +0530

    hi

--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template,request
 
 auth=Blueprint('auth',__name__)
 
-@auth.route('/login')
+@auth.route('/login',methods=['GET','POST'])
 def login():
+    data=request.form
+    print(data)
     return render_template("Login.html")
 
 
